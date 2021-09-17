12118b595b76b960e7497152b9c51590e703a63b
Filip Despic <despicpb12@gmail.com>
2021-09-17 10:23:34 +0200

    add style

--- src/page-load.js
+++ src/page-load.js
@@ -14,13 +14,22 @@ const pageLoad = () => {
     title.innerText = "Restaurant 'TINA'";
     //or like this
     //const txt = document.createTextNode("Restaurant 'Tina'");
-    //para.appendChild(txt);
+    //para.appendChild(txt)
 
     const welcome = document.createElement("h2");
     welcome.innerText = "Welcome to our page!";
 
+    const para = document.createElement("p");
+    para.innerText = "Features: Takeout, Reservations, Outdoor Seating, Seating, Highchairs Available, Wheelchair Accessible, Serves Alcohol, Full Bar, Free Wifi, Accepts Credit Cards, Table Service";
+    const br = document.createElement("br");
+    const para2 = document.createElement("p");
+    para2.innerText = "While some beginner restaurateurs might think that the menu is just a food list, savvy people in the industry know that restaurant menu descriptions make a huge difference. Even researchers were curious if people spend more when the menu descriptions are appetizing. A study from 2019 shows that the menu description triggers 45 % of the buying decision for a specific dish. The same paper identified the main categories of words that have an impact on the client’s decision to order those items. The following factors have the biggest influence for clients selecting a dish";
+    
     content.appendChild(title);
     content.appendChild(welcome);
+    content.appendChild(para);
+    content.appendChild(br);
+    content.appendChild(para2);
 };
 
 export { pageLoad }
